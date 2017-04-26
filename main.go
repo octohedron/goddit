e8f448ba70816e1498fcf8a222312844b15f512f
Gustavo Roríguez <gustavrod@gmail.com>
2017-04-26 23:56:08 +0200

    Adding new pages and oauth reddit login
     - Adding /chat
     - Adding /reddit_callback <- requested from reddit with the code / state
     - Index now it's for logging in with reddit
    
     Adding methods and handlers
      + getRandomString(int) for generating a string
      + getToken(code) string for retrieving an access_token from reddit
      + serveRedditCallback, serveIndex and serveChat for handling requests
    
     Types
      + Adding RedditAuth struct
      + Adding RedditAuth as Auth to User type
      + Adding a map of string -> User for mapping nick/name/id/w/e to a User as session storage / token storage

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"flag"
 	"github.com/gorilla/mux"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"html/template"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"strings"
 	"time"
+)
+
+const (
+	CLIENT_ID     = "1x83QLDFHequ8w"
+	CLIENT_SECRET = "A9R-RZ0kuflGvhR0LJoRYVa9vRE"
+	REDIRECT_URI  = "http://192.168.1.43:9000/reddit_callback"
+	letterBytes   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
 )
 
 type User struct {
@@ -18,7 +33,10 @@ type User struct {
 	Active           string `bson:"active" json:"active"`
 	Activation_token string `bson:"activation_token" json:"activation_token"`
 	Created_at       string `bson:"created_at" json:"created_at"`
+	Auth             RedditAuth
 }
+
+var users map[string]User
 
 type Chatroom struct {
 	Id         bson.ObjectId   `bson:"_id,omitempty" json:"_id,omitempty" inline`
@@ -39,6 +57,15 @@ type Message struct {
 	Timestamp    time.Time     `bson:"timestamp,omitempty" json:"timestamp,omitempty"`
 }
 
+type RedditAuth struct {
+	access_token string
+	token_type   string
+	expires_in   int
+	scope        string
+}
+
+var serverAddress = "http://192.168.1.43:9000/"
+
 var addr = flag.String("addr", ":9000", "http service address")
 
 const project_root = "/home/vagrant/GO/chat"
@@ -46,14 +73,78 @@ const project_root = "/home/vagrant/GO/chat"
 /**
  * Chat channel
  */
-func serveIndex(w http.ResponseWriter, r *http.Request) {
-	// serve
+func serveChat(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		http.Error(w, "Method not allowed", 405)
 		return
 	}
-	t, _ := template.ParseFiles(project_root + "/index.html")
-	t.Execute(w, r)
+
+	template.Must(
+		template.New("chat.html").ParseFiles(
+			project_root+"/chat.html")).Execute(w, struct {
+		Token string
+	}{accessToken})
+}
+
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	state := getRandomString(8)
+	url := "https://ssl.reddit.com/api/v1/authorize?" + "client_id=" + CLIENT_ID +
+		"&response_type=code&state=" + state + "&redirect_uri=" +
+		REDIRECT_URI + "&duration=temporary&scope=identity"
+	log.Println(url)
+	// t, _ := template.ParseFiles(project_root + "/index.html")
+	template.Must(template.New("index.html").ParseFiles(
+		project_root+"/index.html")).Execute(w, struct {
+		Url string
+	}{url})
+}
+
+func serveRedditCallback(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	log.Println(r.URL)
+	err := r.FormValue("error")
+	log.Println("err: " + err)
+	state := r.FormValue("state")
+	log.Println("state: " + state)
+	code := r.FormValue("code")
+	log.Println("code: " + code)
+	accessToken := getToken(code)
+
+	// store reddit auth data in the map
+	log.Println("accessToken: " + accessToken)
+	// redirect to the chat
+	http.Redirect(w, r, serverAddress, 200)
+}
+
+func getToken(code string) string {
+	client := &http.Client{}
+	req, err := http.NewRequest("POST",
+		"https://ssl.reddit.com/api/v1/access_token",
+		bytes.NewBufferString(
+			"grant_type=authorization_code&code="+code+"&redirect_uri="+REDIRECT_URI))
+	req.Header.Add("User-agent", "Reddit-chatterbots")
+	encoded := base64.StdEncoding.EncodeToString(
+		[]byte(CLIENT_ID + ":" + CLIENT_SECRET))
+	// authString = base64.encodestring()
+	// headers = {'Authorization':"Basic %s" % authString}
+	log.Println(encoded)
+	req.Header.Add("Authorization", "Basic "+encoded)
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	redditAuth := RedditAuth{}
+	err = json.Unmarshal(res.Body, &redditAuth)
+	log.Printf("%v", redditAuth)
+	return string(redditAuth.access_token)
 }
 
 /**
@@ -126,12 +217,15 @@ func serveChannelHistory(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// initialize the user slice
+	users = make(map[string]User)
 	flag.Parse()
 	r := mux.NewRouter()
 	hub := newHub()
 	go hub.run()
 	r.HandleFunc("/", serveIndex)
-	// fetch this payload when loading the chat client from web/mobile
+	r.HandleFunc("/chat", serveChat)
+	r.HandleFunc("/reddit_callback", serveRedditCallback)
 	r.HandleFunc("/history/{channel}", serveChannelHistory)
 	r.HandleFunc("/room/{channel}", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
@@ -147,4 +241,24 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+func getRandomString(n int) string {
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
 }
